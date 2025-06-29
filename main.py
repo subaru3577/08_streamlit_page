a72515ed6efb4f918f1d48767a092adbce0726d0
subaru3577 <137596604+subaru3577@users.noreply.github.com>
2025-06-29 16:33:31 +0100

    Update the graph

--- main.py
+++ main.py
@@ -17,5 +17,5 @@ st.write("""
 st.write(df)
 st.line_chart(df)
 st.area_chart(df)
-# myslider = st.slider('Celsius')
-# st.write(myslider, 'in Fahrenheit is', myslider * 9/5 + 32)
+myslider = st.slider('Celsius')
+st.write(myslider, 'in Fahrenheit is', myslider * 9/5 + 32)
