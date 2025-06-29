64bb271e4071ba7f169e0a59dce0ffaa9254c5a0
subaru3577 <137596604+subaru3577@users.noreply.github.com>
2025-06-29 16:09:40 +0100

    First commit

---
+++ main.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.title("First Streamlit App")
+st.subheader("Introducing Streamlit in Automation")
+st.write(""" 
+         Thank you for visiting.
+         This is a sample page with streamlit.
+""")
