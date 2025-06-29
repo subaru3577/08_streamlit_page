08c2662c206b3d8ebc2eb705cd74f602ad826f68
subaru3577 <137596604+subaru3577@users.noreply.github.com>
2025-06-29 16:24:52 +0100

    Added some graphs

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
 import streamlit as st
+import pandas as pd
+
+data = {
+  'Series_1':[1, 3, 4, 5, 7],
+  'Series 2':[10, 30, 40, 100, 250]
+}
+
+df = pd.DataFrame(data)
 
 st.title("First Streamlit App")
 st.subheader("Introducing Streamlit in Automation")
@@ -6,3 +14,8 @@ st.write("""
          Thank you for visiting.
          This is a sample page with streamlit.
 """)
+
+st.line_chart(df)
+st.area_chart(df)
+myslider = st.slider('Celsius')
+st.write(myslider, 'in Fahrenheit is', myslider * 9/5 + 32)
